c220e8afef96932b40bafa6b07fb3acf458a8ff1
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-06-22 13:21:19 +0900

    style: フロントエンドにgraphql-codegenの設定を追加

---
+++ packages/frontend/src/lib/generated/urql-types.ts
@@ -0,0 +1,40 @@
+import gql from "graphql-tag";
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  UUID: { input: string; output: string };
+};
+
+export type Profile = {
+  __typename?: "Profile";
+  age: Scalars["Int"]["output"];
+  name: Scalars["String"]["output"];
+  uuid: Scalars["UUID"]["output"];
+};
+
+export type Query = {
+  __typename?: "Query";
+  user: User;
+};
+
+export type QueryUserArgs = {
+  uuid: Scalars["UUID"]["input"];
+};
+
+export type User = {
+  __typename?: "User";
+  email: Scalars["String"]["output"];
+  profile?: Maybe<Profile>;
+  uuid: Scalars["UUID"]["output"];
+};
