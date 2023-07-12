b8b44c124cf7bbee2fd7fca81aa45b13bb7bc5e6
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-07-12 09:37:42 +0000

    fix: webhookリクエストをgraphql-yogaプラグインへと変更

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -23,8 +23,8 @@ import { authMockOption, authnOption } from "./security/authn";
 // 認可プラグインのオプション
 import { authzOption } from "./security/authz";
 // 開発環境かどうかを判断する変数
-import { isDev, webhook_secret } from "./env";
-import { verifyWebHook } from "./webhook";
+import { isDev } from "./env";
+import { WebHookOnRequest } from "./webhook";
 
 // graphql-armorのプラグインを取得
 const enhancements = armor.protect();
@@ -52,6 +52,14 @@ const yoga = createYoga({
       }
     : false,
   plugins: [
+    // もし開発環境でなければ、webhookの検証を行う
+    ...(isDev
+      ? []
+      : [
+          {
+            onRequest: WebHookOnRequest,
+          },
+        ]),
     // もし開発環境でなければ、introspectionを無効化
     ...(isDev ? [] : [useDisableIntrospection()]),
     // もし開発環境でなければ、graphql-armorを有効化
@@ -64,40 +72,7 @@ const yoga = createYoga({
 });
 
 // yogaサーバーをnodeのhttpサーバーとして起動
-const server = createServer((req, res) => {
-  // もし本番環境でかつ、リクエストがwebhookのエンドポイントであれば
-  if (!isDev && req.url === "/api/loginWebHookEndPoint") {
-    // webhook用のエンドポイントの処理を行う
-
-    // もしリクエスト送信元がlogtoでなければ、401を返す
-    if (req.headers.host !== "logto") {
-      res.writeHead(401);
-      res.end();
-      return;
-    }
-
-    // bodyを取得
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    // bodyを受け取ったら、署名を検証してステータスを返す
-    res.writeHead(
-      verifyWebHook(
-        webhook_secret, // webhookのシークレット
-        body, // リクエストボディ
-        req.headers["logto-signature-sha-256"] as string, // リクエストヘッダーから署名を取得
-        prisma
-      )
-        ? 200
-        : 401
-    );
-    res.end();
-  } else {
-    return yoga;
-  }
-});
+const server = createServer(yoga);
 
 server.listen(4000, () => {
   console.log(`
--- packages/backend/src/webhook.ts
+++ packages/backend/src/webhook.ts
@@ -1,5 +1,7 @@
-import { PrismaClient } from "@prisma/client";
 import { createHmac } from "crypto";
+import { OnRequestEventPayload } from "graphql-yoga/typings/plugins/types";
+import { webhook_secret } from "./env";
+import { GraphQLContext } from "./context";
 
 type WebHookBodyType = {
   // hookId: string;
@@ -26,7 +28,7 @@ type WebHookBodyType = {
 };
 
 // WebHookへのリクエストに対する署名検証を行う関数
-export const verifyWebHook = (signingKey: string, rawBody: string, expectedSignature: string, prisma: PrismaClient) => {
+const verifyWebHook = (signingKey: string, rawBody: string, expectedSignature: string) => {
   // HMAC-SHA256アルゴリズムで署名を生成
   const hmac = createHmac("sha256", signingKey);
   // リクエストボディを更新
@@ -34,20 +36,64 @@ export const verifyWebHook = (signingKey: string, rawBody: string, expectedSigna
   // 正しい署名を16進数の文字列として取得
   const signature = hmac.digest("hex");
   // 期待する署名と一致するかどうかを判定
-  if (signature === expectedSignature) {
-    // bodyのJSONをパース
-    const body = JSON.parse(rawBody) as WebHookBodyType;
+  return signature === expectedSignature;
+};
 
-    // 適するユーザをデータベースに追加
-    prisma.user.create({
-      data: {
-        sub_auth: body.userId,
-        email: body.user.primaryEmail || "dummy@dummy.dummy",
-      },
-    });
+// WebHookのリクエストを処理する関数
+export const WebHookOnRequest = async ({ request, url, fetchAPI, endResponse, serverContext }: OnRequestEventPayload<GraphQLContext>) => {
+  if (serverContext === undefined) {
+    // serverContextが存在しない場合
+    // 何もせず終了
+    endResponse(
+      new fetchAPI.Response("Internal Server Error", {
+        status: 500,
+      })
+    );
+    // リクエストのパスが/api/loginWebHookEndPointの場合
+  } else if (url.pathname === "/api/loginWebHookEndPoint") {
+    // もし送信元ホストがlogtoでない場合は、エラーを返す
+    if (request.headers.get("host") !== "logto.dev") {
+      return endResponse(
+        new fetchAPI.Response("Forbidden", {
+          status: 403,
+        })
+      );
+    }
 
-    return true;
-  } else {
-    return false;
+    // rawBodyを取得
+    const rawBody = await request.text();
+
+    // ヘッダより署名を取得
+    const expectedSignature = request.headers.get("logto-signature-sha-256") || "";
+
+    // 署名検証
+    const isValid = verifyWebHook(webhook_secret, rawBody, expectedSignature);
+
+    if (isValid) {
+      // bodyのJSONをパース
+      const body = JSON.parse(rawBody) as WebHookBodyType;
+
+      // 適するユーザをデータベースに追加
+      serverContext.prisma.user.create({
+        data: {
+          sub_auth: body.userId,
+          email: body.user.primaryEmail || "dummy@dummy.dummy",
+        },
+      });
+
+      // 署名が正しいため、200を返す
+      return endResponse(
+        new fetchAPI.Response("OK", {
+          status: 200,
+        })
+      );
+    } else {
+      // 署名が正しくない場合は、403を返す
+      return endResponse(
+        new fetchAPI.Response("Forbidden", {
+          status: 403,
+        })
+      );
+    }
   }
 };
