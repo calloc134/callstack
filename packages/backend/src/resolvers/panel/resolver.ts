8f0515520c0667f88e50ece178f01bf660045d26
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-06 16:12:27 +0000

    fix: prismaエラー入力先を変更

--- packages/backend/src/resolvers/panel/resolver.ts
+++ packages/backend/src/resolvers/panel/resolver.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { GraphQLContext } from "src/context";
 import { QueryResolvers, UserResolvers, PostResolvers } from "src/lib/generated/resolver-types";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { GraphQLErrorWithCode } from "src/error";
 
 type AsyncFunction<T extends unknown[], R> = (...args: T) => Promise<R>;
