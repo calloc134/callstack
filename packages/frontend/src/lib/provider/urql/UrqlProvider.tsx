c3d4c2fa74f76b3cabda130d8096d495073d09fb
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-11 03:59:53 +0000

    feat: urqlのproviderを切り分け

---
+++ packages/frontend/src/lib/provider/urql/UrqlProvider.tsx
@@ -0,0 +1,71 @@
+import { ReactNode, useState, useCallback } from "react";
+import { Provider, Client, cacheExchange, fetchExchange } from "urql";
+import { authExchange, AuthUtilities, AuthConfig } from "@urql/exchange-auth";
+
+import { isDev, hostname } from "src/env";
+import { useAuthn } from "src/lib/provider/authn/useAuthn";
+
+const UrqlProvider = ({ children }: { children: ReactNode }) => {
+  // urqlクライアントの設定
+  const { isAuthenticated, getAccessToken } = useAuthn();
+  const [jwt, setJwt] = useState<string | undefined>();
+
+  // urqlのauth Exchange用の設定
+  const authInit: (utilities: AuthUtilities) => Promise<AuthConfig> = useCallback(
+    async (utils) => {
+      return {
+        willAuthError() {
+          // e.g. check for expiration, existence of auth etc
+          return !isAuthenticated;
+        },
+        didAuthError(error) {
+          // GraphQLのエラー
+          return error.graphQLErrors.some((e) => e.extensions?.code === "UNAUTHENTICATED");
+        },
+        async refreshAuth() {
+          if (!isAuthenticated) {
+            // 未認証の場合は何もしない
+            return;
+          }
+
+          // トークンの取得
+          const jwt = await getAccessToken();
+          setJwt(jwt);
+        },
+        addAuthToOperation(operation) {
+          // fetchOptionsの適用
+          const fetchOptions = typeof operation.context.fetchOptions === "function" ? operation.context.fetchOptions() : operation.context.fetchOptions || {};
+
+          // ヘッダのオブジェクトの作成
+          const headers = new Headers(fetchOptions.headers);
+          // fetchOptionsによって既にヘッダがある場合は上書きしない
+          if (headers.get("Authorization")) return operation;
+
+          // 認証済みかつjwtがある場合はヘッダに追加
+          if (isAuthenticated && jwt) {
+            return utils.appendHeaders(operation, {
+              // ヘッダの設定
+              Authorization: `Bearer ${jwt}`,
+            });
+          }
+          return operation;
+        },
+      };
+    },
+    [isAuthenticated, getAccessToken, jwt]
+  );
+
+  const urql_client = new Client({
+    // 開発環境であればhttp、本番環境であればhttpsを使う
+    // ホストネームよりフェッチ先のURLを生成
+    url: `${isDev ? "http" : "https"}://${hostname}/api/graphql`,
+    exchanges: [cacheExchange, authExchange(authInit), fetchExchange],
+  });
+
+  return (
+    // urql用のprovider
+    <Provider value={urql_client}>{children}</Provider>
+  );
+};
+
+export { UrqlProvider };
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,69 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider, Client, cacheExchange, fetchExchange } from "urql";
-import { authExchange } from '@urql/exchange-auth';
 import { RouterProvider } from "@tanstack/react-router";
 import { NextUIProvider } from "@nextui-org/react";
 
-import { isDev, hostname } from "./env";
 import { router } from "./route";
-import { AuthnProvider } from "./lib/route/AuthnProvider";
-import { useAuthn } from "./lib/route/useAuthn";
+import { AuthnProvider } from "./lib/provider/authn/AuthnProvider";
 
 export const Main = () => {
-
-  // フックより認証情報の要素を取得
-  const { isAuthenticated, signIn, getAccessToken } = useAuthn();
-
-  const jwt = await getAccessToken();
-
-  
-  // urqlクライアントの設定
-  const urql_client = new Client({
-    // 開発環境であればhttp、本番環境であればhttpsを使う
-    // ホストネームよりフェッチ先のURLを生成
-    url: `${isDev ? "http" : "https"}://${hostname}/api/graphql`,
-    exchanges: [
-      cacheExchange, 
-      authExchange(async utils => {
-        return {
-          addAuthToOperation(operation) {
-            const token = await getAccessToken() || "";
-            if (isAuthenticated && token) {
-              return utils.appendHeaders(operation, {
-                Authorization: `Bearer ${token}`,
-              });
-            }
-
-            return operation;
-          },
-          willAuthError() {
-            // e.g. check for expiration, existence of auth etc
-            return !isAuthenticated;
-          },
-          didAuthError(error, _operation) {
-            // GraphQLのエラー
-            return error.graphQLErrors.some(
-              e => e.extensions?.code === "UNAUTHENTICATED"
-            );
-          },
-          async refreshAuth() {
-            // e.g. refresh token
-          },
-        }
-      }),
-      fetchExchange,
-    ],
-  });
-
   return (
-    // urql用のprovider
-    <Provider value={urql_client}>
-      // NextUI用のprovider
-      <NextUIProvider>
-        <RouterProvider router={router} />
-      </NextUIProvider>
-    </Provider>
+    // NextUI用のprovider
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   );
 };
 
