d0a01065ed4218a4ecfdf948c1db8b234dee869e
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-20 06:07:13 +0000

    refactor: logtoコンフィグの設定にコメントを追加

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -4,8 +4,11 @@ import { logto_endpoint, logto_app_id, logto_api_resource } from "./env";
 // Logtoクライアントの設定
 // 環境変数より取得
 const logto_config: LogtoConfig = {
+  // Logtoのエンドポイント
   endpoint: logto_endpoint,
+  // Logtoのサインインに使用するアプリケーションID
   appId: logto_app_id,
+  // Logtoで認可してもらう対象のAPIリソース
   resources: [logto_api_resource],
 };
 
