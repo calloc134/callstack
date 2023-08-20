7b66dcd9d09ff79ed2c5920198410a861792223a
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-20 11:26:08 +0000

    style: navbarのレスポンシブ対応

--- packages/frontend/src/_document.tsx
+++ packages/frontend/src/_document.tsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import { Outlet, Link } from "@tanstack/react-router";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Avatar, Tooltip, Spacer } from "@nextui-org/react";
-import { Login, Sun } from "tabler-icons-react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  Avatar,
+  Tooltip,
+  Spacer,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { Login, Menu2, Sun } from "tabler-icons-react";
 import { useAuthn } from "./lib/provider/authn/useAuthn";
 
 // 外枠のコンポーネント
@@ -18,17 +31,14 @@ export const Document = () => {
 
   return (
     <div className={`${darkMode ? "dark" : ""} text-foreground bg-background`}>
-      <div className="h-screen flex flex-col">
-        {/* ここで全画面の高さを設定 */}
-        {/* 透明度50% すりガラス風にして影を付ける */}
-        <Navbar className="flex-shrink-0 bg-primary/30 backdrop-blur-sm shadow-xl rounded-b-lg" shouldHideOnScroll>
-          {/* ナビゲーションバーの高さを固定 */}
+      <div className="flex flex-col">
+        <Navbar className="bg-gradient-to-r from-primary bg-primary/30 backdrop-blur-sm shadow-xl rounded-b-lg">
           <NavbarBrand>
             <Tooltip content="callstack" color="secondary">
               <Link to="/">callstack</Link>
             </Tooltip>
           </NavbarBrand>
-          <NavbarContent className="hidden sm:flex gap-4" justify="start">
+          <NavbarContent className="flex gap-4" justify="start">
             <NavbarItem isActive>
               <Link to="/auth/users">ユーザ一覧</Link>
             </NavbarItem>
@@ -37,32 +47,75 @@ export const Document = () => {
             </NavbarItem>
           </NavbarContent>
           <NavbarContent justify="end">
-            <NavbarItem className="sm:flex gap-4">
-              <Tooltip content="スタイル切替" color="secondary">
-                <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
-                  <Sun size={20} />
-                </Button>
-              </Tooltip>
-              {isAuthenticated ? (
-                <Avatar
-                  isBordered
-                  as="button"
-                  className="transition-transform hover:-translate-y-1"
-                  color="secondary"
-                  name="Jason Hughes"
-                  size="sm"
-                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                />
-              ) : (
-                <Tooltip content="ログイン/登録" color="secondary">
-                  <Button color="primary" variant="shadow" className="hover:-translate-y-1">
-                    <Link to="/auth/posts">
-                      <Login size={20} />
-                    </Link>
+            <>
+              {/* 十分に画面サイズが大きい場合 */}
+              <NavbarItem className="hidden sm:flex gap-4">
+                <Tooltip content="スタイル切替" color="secondary">
+                  <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
+                    <Sun size={20} />
                   </Button>
                 </Tooltip>
-              )}
-            </NavbarItem>
+                {isAuthenticated ? (
+                  <Avatar
+                    isBordered
+                    as="button"
+                    className="transition-transform hover:-translate-y-1"
+                    color="secondary"
+                    name="Jason Hughes"
+                    size="sm"
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                  />
+                ) : (
+                  <Tooltip content="ログイン/登録" color="secondary">
+                    <Button color="primary" variant="shadow" className="hover:-translate-y-1">
+                      <Link to="/auth/posts">
+                        <Login size={20} />
+                      </Link>
+                    </Button>
+                  </Tooltip>
+                )}
+              </NavbarItem>
+              {/* 画面サイズが小さい場合 */}
+              <NavbarItem className="flex sm:hidden gap-4">
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button color="secondary" variant="light" className="hover:-translate-y-1">
+                      <Menu2 size={20} />
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu>
+                    <DropdownItem>
+                      <Tooltip content="スタイル切替" color="secondary">
+                        <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
+                          <Sun size={20} />
+                        </Button>
+                      </Tooltip>
+                    </DropdownItem>
+                    <DropdownItem>
+                      {isAuthenticated ? (
+                        <Avatar
+                          isBordered
+                          as="button"
+                          className="transition-transform hover:-translate-y-1"
+                          color="secondary"
+                          name="Jason Hughes"
+                          size="sm"
+                          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                        />
+                      ) : (
+                        <Tooltip content="ログイン/登録" color="secondary">
+                          <Button color="primary" variant="shadow" className="hover:-translate-y-1">
+                            <Link to="/auth/posts">
+                              <Login size={20} />
+                            </Link>
+                          </Button>
+                        </Tooltip>
+                      )}
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavbarItem>
+            </>
           </NavbarContent>
         </Navbar>
         <div className="flex-grow overflow-auto">
